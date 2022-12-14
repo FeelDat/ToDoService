dc22788957aaa38c3c3c7acabd4b224b28e4a5f4
FeelDat <alimaldybergenov@gmail.com>
2022-12-14 16:21:44 +0600

    Help, eureka doesnt work

--- src/main/java/ToDoService/ToDoServiceApplication.java
+++ src/main/java/ToDoService/ToDoServiceApplication.java
@@ -2,10 +2,10 @@ package ToDoService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
-
 public class ToDoServiceApplication {
 
     public static void main(String[] args) {
